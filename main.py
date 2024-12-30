22502de3a7a853c5736c829a6cc820c5d642601b
bucketpigeon <mt.skybox@gmail.com>
2024-12-29 23:18:54 -0800

    Got the stupid triangle on my screen

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(
+            SCREEN_WIDTH / 2,
+            SCREEN_HEIGHT / 2,
+            PLAYER_RADIUS
+            )
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-
+        dt = clock.tick(60) / 1000
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
