be2c4ed75d2d5637228bef64f8e5fa6e185d7431
bucketpigeon <mt.skybox@gmail.com>
2025-01-21 23:07:23 -0800

    Working moving and rotating player triangle

--- main.py
+++ main.py
@@ -1,29 +1,32 @@
 import pygame
 from constants import *
-from player import *
+from player import Player
+
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
-    player = Player(
-            SCREEN_WIDTH / 2,
-            SCREEN_HEIGHT / 2,
-            PLAYER_RADIUS
-            )
 
+    #updatable = pygame.sprite.Group()
+    #drawable = pygame.sprite.Group()
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        dt = clock.tick(60) / 1000
+
+        player.update(dt)
+
         screen.fill("black")
         player.draw(screen)
         pygame.display.flip()
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
